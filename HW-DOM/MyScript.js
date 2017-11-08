e9e06e8e44b3f6c1dfb9394e722b8668d6662584
benrabaa <benrabaa@gmail.com>
2017-11-08 14:31:48 -0500

    adding Dom hw

---
+++ HW-DOM/MyScript.js
@@ -0,0 +1,5 @@
+/* This script to write this text "I am from JavaScript file" in H1 element*/
+/* assign h1element Varibale to h1 element by Head1 ID */
+var h1element= document.getElementById("head1") 
+/* assign this text "I am from JavaScript file" to h1 element by h1element variable */
+h1element.innerHTML="I am from JavaScript file"; 
