6e463f8ca28d7f2baae7e90f58ef51fc9f152131
benrabaa <benrabaa@gmail.com>
2017-11-12 00:17:29 -0500

    Prompt Assignment

---
+++ Prompt-HomeWork/prompt.js
@@ -0,0 +1,4 @@
+var input =prompt("Enter name"); //Assign  answer of the prompt screen to variable(INPUT)
+console.log(input);
+			
+document.getElementById("MyName").innerHTML=input;  //Assign variable(INPUT) vale to element id MyName(H1) which is the answer from prompt
